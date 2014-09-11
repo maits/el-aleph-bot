69393daf074580fec8e71c0a88492205549f381d
Maite Fernandez <maite.fern@gmail.com>
2014-09-11 19:26:55 -0400

    Update code.py

--- code.py
+++ code.py
@@ -9,13 +9,11 @@ ACCESS_TOKEN_SECRET = 'XXXXXXXXXXXXX'
 
 twitter = Twython(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-#twitter.update_status(status= "")
 
 filename=open('helloworld.txt', 'r')
 f=filename.readlines()
 filename.close()
 
-#there's a problem here
 for line in f:
 	if len(line) < 140:
     		twitter.update_status(status=line)
